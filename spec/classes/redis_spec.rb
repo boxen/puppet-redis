586531fbdf885a809d1fbc01ae09ebff1296afde
Gary Larizza <gary@puppetlabs.com>
2013-05-03 15:04:22 -0700

    Add spec tests for Darwin
    Previously the rspec-puppet tests were only applicable for Debian. This
    commit introduces minimal tests for Darwin (OS X).

--- spec/classes/redis_spec.rb
+++ spec/classes/redis_spec.rb
@@ -1,29 +1,60 @@
 require 'spec_helper'
 
 describe 'redis' do
-  let(:facts) {
-    {
+  context 'Debian' do
+    let(:facts) {
+      {
       :osfamily => 'Debian'
     }
-  }
-  let(:configfile) { '/etc/redis.conf' }
-  let(:package_name) { 'redis-server' }
-  let(:service_name) { 'redis-server' }
-  let(:version) { 'installed' }
+    }
+    let(:configfile) { '/etc/redis.conf' }
+    let(:package_name) { 'redis-server' }
+    let(:service_name) { 'redis-server' }
+    let(:version) { 'installed' }
 
-  it do
-    should contain_file(configfile).with({
-      :notify => 'Service[redis]'
-    })
-    should contain_package('redis').with({
-      :ensure => version,
-      :name   => package_name,
-      :notify => 'Service[redis]'
-    })
-    should include_class('redis::config')
-    should contain_service('redis').with({
-      :ensure  => 'running',
-      :name    => service_name
-    })
+    it do
+      should contain_file(configfile).with({
+        :notify => 'Service[redis]'
+      })
+      should contain_package('redis').with({
+        :ensure => version,
+        :name   => package_name,
+        :notify => 'Service[redis]'
+      })
+      should include_class('redis::config')
+      should contain_service('redis').with({
+        :ensure  => 'running',
+        :name    => service_name
+      })
+        end
   end
+  context 'Darwin' do
+    let(:facts) {
+      {
+      :osfamily   => 'Darwin',
+      :boxen_home => '/opt/boxen'
+      }
+    }
+    let(:configfile) { "#{facts[:boxen_home]}/config/redis/redis.conf" }
+    let(:package_name) { 'boxen/brews/redis' }
+    let(:service_name) { 'dev.redis' }
+    let(:version) { '2.6.9-boxen1' }
+
+    it do
+      should contain_file(configfile).with({
+        :notify => 'Service[redis]'
+      })
+      should contain_package('redis').with({
+        :ensure => version,
+        :name   => package_name,
+        :notify => 'Service[redis]'
+      })
+      should include_class('redis::config')
+      should contain_service('redis').with({
+        :ensure  => 'running',
+        :name    => service_name
+      })
+    end
+  end
+
 end
